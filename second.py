86fcf6405fb7c2f3d0c0a00ad22bc42f9b624b6a
Jignesh Purohit <156500596+Kakashi-13@users.noreply.github.com>
2024-01-21 14:29:23 +0530

    Add files via upload
     A program that calculates the area and perimeter of a rectangle.

---
+++ second.py
@@ -0,0 +1,11 @@
+fh =open('output2.txt','w')
+length = int(input("Enter the length of Rectangle : "))
+breath = int(input("Enter the breath of Rectangle : "))
+
+Area = length * breath
+Perameter = 2 * (length + breath)
+
+fh.write("Area of Rectangle is :" + str(Area)+'\n')
+fh.write("Perimeter of Rectangle is :" + str(Perameter) + '\n')
+fh.write("---------------------------------------------")
+fh.close
